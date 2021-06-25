05ec73a7bd4c83934a0d6a5b350fd90499f95114
siavava <amittaijoel@outlook.com>
2021-06-25 16:52:16 -0400

    create populate

--- tests/populatedb.sql
+++ tests/populatedb.sql
@@ -5,3 +5,9 @@ CREATE TABLE IF NOT EXISTS donut_list
   donut_type VARCHAR(6),
   fun_fact VARCHAR(10)
 );
+INSERT INTO donut_list (donut_name, donut_type, fun_fact)
+VALUES (
+    'KDF',
+    'hard',
+    'tough!'
+  );
