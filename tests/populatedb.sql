2e8710cc27824225b5d3fdbc0233b8710fdce268
siavava <amittaijoel@outlook.com>
2021-06-26 10:35:44 -0400

    select, insert

--- tests/populatedb.sql
+++ tests/populatedb.sql
@@ -6,8 +6,6 @@ CREATE TABLE IF NOT EXISTS donut_list
   fun_fact VARCHAR(10)
 );
 INSERT INTO donut_list (donut_name, donut_type, fun_fact)
-VALUES (
-  'KDF',
-  'hard',
-  'tough!'
-);
+VALUES 
+  ('KDF', 'hard', 'tough!'),
+  ('Mandazi', 'soft', 'easy');
